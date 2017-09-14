1d9e6f6498c34826795b36dec3a664dd1a0574a3
Ryan Scott <ryan.gl.scott@gmail.com>
2017-09-13 22:12:17 -0400

    DerivingVia

---
+++ testsuite/tests/deriving/should_fail/deriving-via-fail.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module DerivingViaFail where
+
+import Control.Category
+import Data.Functor.Identity
+
+newtype Foo1 a = Foo1 a deriving Show via (Identity b)
+
+newtype Foo2 a b = Foo2 (a -> b)
+  deriving Category
+    via fooo
+
+data Foo3 deriving Eq via (forall a. a)
