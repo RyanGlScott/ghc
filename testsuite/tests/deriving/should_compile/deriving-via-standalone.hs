6f75b23818b06aab6960ae343d1ac04e05a6b2f8
Ryan Scott <ryan.gl.scott@gmail.com>
2018-03-13 20:23:14 -0400

    Fix #28 with yet more unification
    A bit ugly, but it gets the job done.

--- testsuite/tests/deriving/should_compile/deriving-via-standalone.hs
+++ testsuite/tests/deriving/should_compile/deriving-via-standalone.hs
@@ -1,20 +1,28 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module DerivingViaStandalone where
 
 import Control.Applicative
 import Data.Functor.Compose
+import Data.Proxy
 import Data.Semigroup
 
-newtype App f a = App (f a)
+newtype App (f :: * -> *) a = App (f a)
   deriving newtype
     (Functor, Applicative)
 
 instance (Applicative f, Semigroup a) => Semigroup (App f a) where
   (<>) = liftA2 (<>)
 
-deriving via (App (Compose f g) a)
+deriving via (App (Compose (f :: * -> *) g) a)
          instance (Applicative f, Applicative g, Semigroup a)
                => Semigroup (Compose f g a)
+
+class C (a :: k -> *)
+instance C Proxy
+
+newtype MyProxy a = MyProxy (Proxy a)
+deriving via (Proxy :: * -> *) instance C MyProxy
